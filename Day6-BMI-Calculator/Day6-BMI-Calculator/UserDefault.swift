7fde923ccd71a0c6f87ca14c6631c439d58eaedd
Taeyoung Won <hexter1994@gmail.com>
2024-01-05 11:17:26 +0900

    [Feat] 저장된 히스토리 삭제 기능 구현

--- Day6-BMI-Calculator/Day6-BMI-Calculator/UserDefault.swift
+++ Day6-BMI-Calculator/Day6-BMI-Calculator/UserDefault.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+enum Key: String, CaseIterable {
+  case nickname
+  case height
+  case weight
+  
+  var name: String {
+    return self.rawValue
+  }
+}
+
 struct User {
   @UserDefault(key: .nickname, defaultValue: "사용자") 
   var nickname: String
@@ -14,13 +24,21 @@ struct User {
   var height: Int
   @UserDefault(key: .weight, defaultValue: 0) 
   var weight: Int
+  
+  func removeHistory() {
+    let manager = UserDefaults.standard
+    
+    Key.allCases.forEach { key in
+      manager.removeObject(forKey: key.name)
+    }
+  }
 }
 
-// 프로퍼티 래퍼를 통해 중복되는 로직인 저장 / 불러오기를 구현
-// UserDefault를 사용하는 값들에 해당 래퍼를 선언하여 구현된 로직을 일괄적으로 재사용
+/// 프로퍼티 래퍼를 통해 중복되는 로직인 저장 / 불러오기를 구현
+/// UserDefault를 사용하는 값들에 해당 래퍼를 선언하여 구현된 로직을 일괄적으로 재사용
 @propertyWrapper
 struct UserDefault<T: Any> {
-  private let key: Key
+  let key: Key
   private var defaultValue: T
   
   init(
@@ -37,16 +55,6 @@ struct UserDefault<T: Any> {
     }
     set {
       UserDefaults.standard.set(newValue, forKey: key.name)
-    }
-  }
-  
-  enum Key: String {
-    case nickname
-    case height
-    case weight
-    
-    var name: String {
-      return self.rawValue
     }
   }
 }
